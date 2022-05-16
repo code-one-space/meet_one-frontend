33d7120481ed83cc6f3a117cfbdcfbcbf2840697
nojomyth-dev <52127809+nojomyth-dev@users.noreply.github.com>
2022-05-16 21:15:05 +0200

    Update index.js

--- components/shareButton/index.js
+++ components/shareButton/index.js
@@ -1,7 +1,7 @@
 import {TouchableOpacity, ImageBackground, View, Text, Image} from "react-native";
 import shareButtonStyle from "./shareButton.style";
 
-export default function Index ({ navigation }) {
+export default function ShareButton ({ navigation }) {
     return (
             <TouchableOpacity onPress={() => navigation.navigate("ShareScreen")}>
                 <View style={shareButtonStyle.container}>
