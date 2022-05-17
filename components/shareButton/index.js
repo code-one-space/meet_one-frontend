cc0f8d0c49f1de26543fca2c2af60d81171f4eeb
Janik Franke <janikfranke@googlemail.com>
2022-05-17 18:26:07 +0200

    renamed Navigation to navigation

--- components/shareButton/index.js
+++ components/shareButton/index.js
@@ -1,10 +1,10 @@
 import {TouchableOpacity, ImageBackground, View, Text, Image} from "react-native";
 import shareButtonStyle from "./shareButton.style";
-import * as Navigation from "Navigation";
+import * as navigation from "Navigation";
 
 export default function ShareButton () {
     return (
-            <TouchableOpacity onPress={() => Navigation.navigate("ShareScreen") }>
+            <TouchableOpacity onPress={() => navigation.navigate("ShareScreen") }>
                 <View style={shareButtonStyle.container}>
                     <Image source={require("@@assets/shareIcon.png")} style={shareButtonStyle.image}/>
                 </View>
