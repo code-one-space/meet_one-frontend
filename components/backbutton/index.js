4fe6c13d2b260a502659c8ae173f54e144ac9fc1
immi <\_()_/>
2022-05-19 13:10:55 +0200

    backButton white prop

--- components/backbutton/index.js
+++ components/backbutton/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
 import buttonStyle from './backbutton.style';
 
-export default function BackButton ({ onPress }) {
+export default function BackButton ({ onPress, white = true }) {
+    const imageSource = white ? "@@assets/BackButtonWhite.png" : "@@assets/BackButtonBlack.png"
     return (
         <View>
         <TouchableOpacity style={buttonStyle.button} onPress={onPress}>
-            <Image source={require("@@assets/Ebene_2_white.png")} style = {buttonStyle.foto}/>
+            <Image source={require(imageSource)} style = {buttonStyle.foto}/>
         </TouchableOpacity>
         </View>
     );
