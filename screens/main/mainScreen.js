e7b5e87c9eb6260c6fd001771dcd18f98b96af03
acasor <janikfranke@googlemail.com>
2022-05-21 12:50:15 +0200

    hardware back button handling confirm screen

--- screens/main/mainScreen.js
+++ screens/main/mainScreen.js
@@ -1,17 +1,36 @@
 import { Text, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
-import { Button, ShareButton, BackButton } from "@@components";
+import { Button } from "@@components";
+import { BackHandler } from "react-native";
+import * as Navigation from "../../Navigation";
 
-export default function MainScreen ({ navigation }) {
+export default function MainScreen ({ navigation, route }) {
+
+    const handleBackButton = () => {
+        if (Navigation.getCurrentRouteName() === "MainScreen") {
+            callConfirmScreen(navigation);
+            return true;
+        }
+
+        BackHandler.exitApp();
+        return true;
+    }
+
+    BackHandler.addEventListener('hardwareBackPress', handleBackButton);
+
     return (
         <View>
             <Text>This is the MainScreen</Text>
             <StatusBar style="auto" />
-            <Button title={"Leave Team"} onPress={() => navigation.navigate(
-                "ConfirmScreen",
-                { message: "Do you want to leave the Team?", followingScreen: "StartScreen" }
-            )}/>
+            <Button title={"Leave Team"} onPress={() => callConfirmScreen(navigation)}/>
             <Button title={"Add Tool"} onPress={() => navigation.navigate("SelectToolScreen")}/>
         </View>
+    )
+}
+
+const callConfirmScreen = navigation => {
+    navigation.navigate(
+        "ConfirmScreen",
+        { message: "Do you want to leave the Team?", followingScreen: "StartScreen" }
     )
 }
