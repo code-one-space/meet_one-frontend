87301ecf51e6fdd4fa16672e15b54e5a4d9b6a1b
nojomyth-dev <nojomyth@gmx.de>
2022-05-30 15:15:37 +0200

    Added basic eslint stuff

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,16 @@
 module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: [
-      ["module-resolver",
-        {
-          root: ["."],
-          alias: {
-            "^@@(.+)": "./\\1"
-          }
-        }
-      ]
-    ]
-  };
+    api.cache(true);
+    return {
+        presets: ['babel-preset-expo'],
+        plugins: [
+            ["module-resolver",
+                {
+                    root: ["."],
+                    alias: {
+                        "^@@(.+)": "./\\1"
+                    }
+                }
+            ]
+        ]
+    };
 };
