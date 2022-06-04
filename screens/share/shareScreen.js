d541f9c43ebbb38e94ffc59f26b47628ef48e539
acasor <janikfranke@googlemail.com>
2022-06-04 16:49:04 +0200

    black background, share link button

--- screens/share/shareScreen.js
+++ screens/share/shareScreen.js
@@ -3,18 +3,20 @@ import { StatusBar } from "expo-status-bar";
 import QRCode from "react-native-qrcode-svg";
 import * as HttpClient from "../../shared/httpClient/httpClient"
 import styles from "./shareScreen.style";
+import { Button } from "@@components";
 
 export default function ShareScreen ({ navigation }) {
     return (
         <SafeAreaView style={styles.container}>
             <StatusBar style="auto" />
-            <View style={styles.outerQrContainer}>
-                <View style={styles.innerQrContainer}>
-                    <QRCode
-                        size={300}
-                        value={"codeone_meetingapp_id:" + HttpClient.meetingId}
-                    />
-                </View>
+            <View style={styles.qrContainer}>
+                <QRCode
+                    size={250}
+                    value={"codeone_meetingapp_id:" + HttpClient.meetingId}
+                />
+            </View>
+            <View style={styles.buttonContainer}>
+                <Button title={"Share Link"} white={true}/>
             </View>
         </SafeAreaView>
     )
