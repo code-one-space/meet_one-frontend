384894365abce6038828a3717399103e32d364fb
acasor <janikfranke@googlemail.com>
2022-06-06 18:09:30 +0200

    link is shared now

--- screens/share/shareScreen.js
+++ screens/share/shareScreen.js
@@ -1,5 +1,6 @@
 import {View, SafeAreaView, Share} from "react-native";
 import { StatusBar } from "expo-status-bar";
+import * as Linking from "expo-linking";
 import QRCode from "react-native-qrcode-svg";
 import * as HttpClient from "../../shared/httpClient/httpClient"
 import styles from "./shareScreen.style";
@@ -8,7 +9,8 @@ import { Button } from "@@components";
 export default function ShareScreen ({ navigation }) {
     const handleShareLink = async () => {
         try {
-            await Share.share({ message: "This is the share message" });
+            let link = Linking.createURL("", { queryParams: { meetingId: HttpClient.meetingId }});
+            await Share.share({ message: "Join my Meeting by clicking on this Link:\n" + link });
         } catch (error) {
             console.error(error);
             alert(error);
