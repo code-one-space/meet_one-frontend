645e1b820d7303dd65a3fe52cd6e95bb203fe368
janik <janikfranke@googlemail.com>
2022-06-21 11:15:50 +0200

    removed icon styles

--- components/editChoiceListItem/index.js
+++ components/editChoiceListItem/index.js
@@ -7,10 +7,10 @@ export default function EditChoiceListItem({ choice }) {
         <View style={style.container}>
             <TouchableOpacity style={style.editButton}>
                 <Text style={style.text}>{choice}</Text>
-                <Edit stroke="black" fill="white" width={25} height={25} style={style.editIcon}/>
+                <Edit stroke="black" fill="white" width={25} height={25}/>
             </TouchableOpacity>
             <View style={style.trashContainer}>
-                <Trash2 stroke="white" fill="#000000" width={25} height={25} style={style.trashIcon}/>
+                <Trash2 stroke="white" fill="#000000" width={25} height={25}/>
             </View>
         </View>
     )
