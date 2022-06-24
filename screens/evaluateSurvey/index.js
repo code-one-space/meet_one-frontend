7d93137f804d1832595d7d0f17741c39a5a5c579
Janik Franke <janikfranke@googlemail.com>
2022-06-24 20:31:38 +0200

    removed sample data

--- screens/evaluateSurvey/index.js
+++ screens/evaluateSurvey/index.js
@@ -3,33 +3,7 @@ import {SurveyAnswerListItem} from "@@components";
 import {SafeAreaView, Text, View, FlatList} from "react-native";
 
 export default function EvaluateSurveyScreen({ route }) {
-    // const { survey } = route.params;
-
-    const survey = {
-        question: "How are you today?",
-        answers: [
-            {
-                memberName: "Janik",
-                answer: "Not bad!",
-            },
-            {
-                memberName: "Max Mustermann",
-                answer: "I hate people",
-            },
-            {
-                memberName: "Erika Musterfrau",
-                answer: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
-            },
-            {
-                memberName: "Erika Musterfrau",
-                answer: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
-            },
-            {
-                memberName: "Erika Musterfrau",
-                answer: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
-            }
-        ]
-    }
+    const { survey } = route.params;
 
     function renderItem({ item }) {
         return <SurveyAnswerListItem answer={item}/>
