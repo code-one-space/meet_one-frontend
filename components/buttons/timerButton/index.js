b61cd6054f7ff3ea9c4268f5b89cc6c3d29572fe
janik <janikfranke@googlemail.com>
2022-06-28 15:47:48 +0200

    fixed timerButton style

--- components/buttons/timerButton/index.js
+++ components/buttons/timerButton/index.js
@@ -4,19 +4,15 @@ import * as Icon from "react-native-feather";
 import React from "react";
 
 
-export default function TimerButton ({ onPress,time}) {
+export default function TimerButton ({ onPress, time }) {
 
 
     return (
         <TouchableOpacity onPress={onPress}>
 
             <View style={style.container}>
-
-                <View style={style.innerContainer}>
-                    <Icon.Clock stroke="white" fill="#000000" width={25} height={25} style = {style.icon}/>
+                    <Icon.Clock stroke="black" fill="white" width={30} height={30} style = {style.icon}/>
                     <Text style={style.text}>{time}</Text>
-                </View>
-
             </View>
 
         </TouchableOpacity>
